03d3384b15fad6c5349ac6e953ebc6f277b3cc9d
Tucker K <tucker@Tuckers-MacBook-Pro.local>
2022-03-22 13:01:39 -0500

    tt

---
+++ bookshelf/functions/getBookshelf.js
@@ -0,0 +1,22 @@
+exports = function(arg){
+  /*
+    Accessing application's values:
+    var x = context.values.get("value_name");
+
+    Accessing a mongodb service:
+    var collection = context.services.get("mongodb-atlas").db("dbname").collection("coll_name");
+    collection.findOne({ owner_id: context.user.id }).then((doc) => {
+      // do something with doc
+    });
+
+    To call other named functions:
+    var result = context.functions.execute("function_name", arg1, arg2);
+
+    Try running in the console below.
+  */
+  
+  const mongodb = context.services.get("mongodb-atlas");
+  const mycollection = mongodb.db("bookshelf_test").collection("bookshelf");
+  
+  return mycollection.find({}).limit(10).toArray();
+};
